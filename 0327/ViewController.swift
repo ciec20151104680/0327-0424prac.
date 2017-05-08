fae15011a127d1fa81981c7639e38cc789f809eb
ciec20151104680 <cxxxe4680@Mac28.local>
2017-05-08 10:44:18 +0800

    比赛计分器 5th修改
    比赛计分器 5th 修改

--- 0327/ViewController.swift
+++ 0327/ViewController.swift
@@ -52,4 +52,33 @@ class ViewController: UIViewController {
         }
         
 }
+    @IBAction func addb(_ sender: Any) {
+        b=b+1
+        if( a<10 && b==11 || a>=10 && b-a==2)
+        {
+            temp.text="\("第" + String(c) + "局b胜" + String(a) + ":" + String(b))"
+            array1[c-1] = a
+            array2[c-1] = b
+            c=c+1
+            a=0
+            b=0
+            e=e+1
+            temp2.text = "\(e)"
+            if (c == 4)
+            {
+                temp4.text="\("比赛结束" + String(d) + ":" + String(e))"
+                temp.text="\( "b赢  " )"
+                c=1;
+                d=0;
+                e=0;
+                temp2.text = "\(e)"
+                temp1.text="\(d)"
+            }
+        }else
+        {
+            temp.text="\( String(a) + ":" + String(b) )"
+            temp4.text="\("第" + String(c) + "局")"
+        }
+}
+
 
