4d4d2763d2211f5befe25e27afc762f266f7ddda
ciec20151104680 <cxxxe4680@Mac28.local>
2017-05-17 12:00:17 +0800

    8th

--- GameNew/比赛记分器/比赛记分器/ViewController.swift
+++ GameNew/比赛记分器/比赛记分器/ViewController.swift
@@ -26,7 +26,7 @@ class ViewController: UIViewController {
         a=a+1
         if( b<10 && a==11 || b>=10 && a-b==2 )
         {
-            temp.text="\("第" + String(c) + "局a胜" + String(a) + ":" + String(b))"
+            temp2.text="\("第" + String(c) + "局a胜" + String(a) + ":" + String(b))"
             array1[c-1] = a
             array2[c-1] = b
             c=c+1
@@ -47,7 +47,35 @@ class ViewController: UIViewController {
             }
         }else
         {
-            temp.text="\( String(a) + ":" + String(b) )"
+            temp2.text="\( String(a) + ":" + String(b) )"
+            temp1.text="\("第" + String(c) + "局")"
+        }
+    }
+    @IBAction func b1(_ sender: Any) {
+        b=b+1
+        if( a<10 && b==11 || a>=10 && b-a==2)
+        {
+            temp1.text="\("第" + String(c) + "局b胜" + String(a) + ":" + String(b))"
+            array1[c-1] = a
+            array2[c-1] = b
+            c=c+1
+            a=0
+            b=0
+            e=e+1
+            temp2.text = "\(e)"
+            if (c == 4)
+            {
+                temp0.text="\("比赛结束" + String(d) + ":" + String(e))"
+                temp1.text="\( "b赢  " )"
+                c=1;
+                d=0;
+                e=0;
+                temp2.text = "\(e)"
+                temp1.text="\(d)"
+            }
+        }else
+        {
+            temp2.text="\( String(a) + ":" + String(b) )"
             temp1.text="\("第" + String(c) + "局")"
         }
     }
